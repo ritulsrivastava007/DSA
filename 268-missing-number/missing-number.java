b4fb3993ec7294eab58ee9acd0bb432f5b5ebc05
Ritul <ritul.srivastava007@gmail.com>
2026-04-28 20:41:23 +0530

    Fix missing number logic in missing-number.java

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
-
         for(int i=0;i<nums.length;i++){
                 if(nums[i]!=i){
                     return i;
