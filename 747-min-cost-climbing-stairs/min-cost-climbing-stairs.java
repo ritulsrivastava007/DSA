64381217176a70bc09b08cbe235e3a882a10da5d
Ritul <ritul.srivastava007@gmail.com>
2026-05-06 19:28:49 +0530

    Time: 1 ms (38.20%) | Memory: 45 MB (23.32%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -9,6 +9,7 @@ class Solution {
         for (int i = 2; i < n; i++) {
             dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
         }
+        
         return Math.min(dp[n-1], dp[n-2]);
     }
 }
