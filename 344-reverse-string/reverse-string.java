c69f491af454cd04f0ee2f9da47931cfb447ea8d
Ritul <ritul.srivastava007@gmail.com>
2026-05-11 19:29:46 +0530

    Time: 1 ms (21.83%) | Memory: 48.2 MB (72.97%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char[] rev = new char[s.length];
+
+        for (int i = 0; i < s.length; i++) {
+            rev[i] = s[s.length - 1 - i];
+        }
+        for (int i = 0; i < s.length; i++) {
+            s[i] = rev[i];
+        }
+    }
+}
