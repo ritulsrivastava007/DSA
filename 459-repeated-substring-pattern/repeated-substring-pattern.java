cea44943df5d5605c62edbbd871f3463ce3273f3
Ritul <ritul.srivastava007@gmail.com>
2026-05-13 19:33:46 +0530

    Time: 30 ms (79.31%) | Memory: 46.6 MB (83.71%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,6 +1,19 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String doubled = s + s;
-        return doubled.substring(1, doubled.length() - 1).contains(s);
+        int n = s.length();
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len == 0) {
+                String sub = s.substring(0, len);
+                StringBuilder sb = new StringBuilder();
+                int times = n / len;
+                for (int i = 0; i < times; i++) {
+                    sb.append(sub);
+                }
+                if (sb.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
