59fabd72b319cbc75f5ffee6ed23638c7238cc01
Ritul <ritul.srivastava007@gmail.com>
2026-05-14 18:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.65%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,15 +1,11 @@
 class Solution {
     public int findComplement(int num) {
-        String binary = Integer.toBinaryString(num);
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < binary.length(); i++) {
-            if (binary.charAt(i) == '1') {
-                sb.append('0');
-            } else {
-                sb.append('1');
-            }
+        int mask = 1;
+        
+        while (mask < num) {
+            mask = (mask << 1) | 1;
         }
-        return Integer.parseInt(sb.toString(), 2);
+
+        return mask ^ num;
     }
 }
