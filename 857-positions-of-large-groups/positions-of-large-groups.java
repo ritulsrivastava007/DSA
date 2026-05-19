240adc2ce8d0c1d618c2fe4de939ea14fc639b95
Ritul <ritul.srivastava007@gmail.com>
2026-05-19 19:08:49 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (71.29%) - LeetSync

--- 857-positions-of-large-groups/positions-of-large-groups.java
+++ 857-positions-of-large-groups/positions-of-large-groups.java
@@ -1,29 +1,16 @@
 import java.util.*;
+
 class Solution {
     public List<List<Integer>> largeGroupPositions(String s) {
         List<List<Integer>> ans = new ArrayList<>();
-        int freq = 1;
         int start = 0;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                freq++;
-            } 
-            else {
-                if (freq >= 3) {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(start);
-                    temp.add(i - 1);
-                    ans.add(temp);
+        for (int i = 1; i <= s.length(); i++) {
+            if (i == s.length() || s.charAt(i) != s.charAt(i - 1)) {
+                if (i - start >= 3) {
+                    ans.add(Arrays.asList(start, i - 1));
                 }
                 start = i;
-                freq = 1;
             }
-        }
-        if (freq >= 3) {
-            List<Integer> temp = new ArrayList<>();
-            temp.add(start);
-            temp.add(s.length() - 1);
-            ans.add(temp);
         }
         return ans;
     }
