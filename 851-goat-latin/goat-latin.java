e3bbafd8e906ac211b9b61683262c900f0417705
Ritul <ritul.srivastava007@gmail.com>
2026-05-19 18:58:24 +0530

    Time: 2 ms (91.51%) | Memory: 43.2 MB (64.83%) - LeetSync

--- 851-goat-latin/goat-latin.java
+++ 851-goat-latin/goat-latin.java
@@ -1,28 +1,18 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public String toGoatLatin(String sentence) {
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-        vowels.add('A');
-        vowels.add('E');
-        vowels.add('I');
-        vowels.add('O');
-        vowels.add('U');
         String[] words = sentence.split(" ");
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
-            if (vowels.contains(word.charAt(0))) {
+            char ch = word.charAt(0);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                 ans.append(word);
             } else {
-                ans.append(word.substring(1));
-                ans.append(word.charAt(0));
+                for (int j = 1; j < word.length(); j++) {
+                    ans.append(word.charAt(j));
+                }
+                ans.append(ch);
             }
             ans.append("ma");
             for (int j = 0; j <= i; j++) {
