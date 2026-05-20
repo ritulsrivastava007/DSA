1bcfca9e30925fdfb0172ac8600774777770f324
Ritul <ritul.srivastava007@gmail.com>
2026-05-20 19:29:54 +0530

    Time: 3 ms (18.57%) | Memory: 85.2 MB (37.11%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        boolean inc = true, dec = true;
+        boolean inc = true;
+        boolean dec = true;
         for (int i = 1; i < nums.length; i++) {
-            inc &= nums[i] >= nums[i - 1];
-            dec &= nums[i] <= nums[i - 1];
+            if (nums[i] > nums[i - 1]) {
+                dec = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                inc = false;
+            }
         }
         return inc || dec;
     }
