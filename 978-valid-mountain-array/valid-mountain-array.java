f32c5557b111ad4a34ed3ea36fc2b65b0706fb73
Ritul <ritul.srivastava007@gmail.com>
2026-05-23 19:06:19 +0530

    Time: 2 ms (86.23%) | Memory: 47.3 MB (86.34%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        int idx = IntStream.range(0, arr.length).reduce((i, j) -> arr[i] > arr[j] ? i : j).getAsInt();
-        if (idx == 0 || idx == arr.length - 1) return false;
-        int start = 0, end = arr.length - 1;
-        for (int i = 0; i < idx; i++)
-            if (arr[i] >= arr[i + 1]) return false;
-            
-        for (int i = idx; i < arr.length - 1; i++)
-            if (arr[i] <= arr[i + 1]) return false;
-
-        return true;
+        int i = 0, n = arr.length;
+        while (i + 1 < n && arr[i] < arr[i + 1]) i++;
+        if (i == 0 || i == n - 1) return false;
+        while (i + 1 < n && arr[i] > arr[i + 1]) i++;
+        return i == n - 1;
     }
 }
