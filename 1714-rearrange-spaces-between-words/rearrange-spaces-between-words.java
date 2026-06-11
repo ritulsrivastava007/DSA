0f3fc890992e798a8e9ead08cd44b94e7c6de602
Ritul <ritul.srivastava007@gmail.com>
2026-06-11 19:20:19 +0530

    Time: 3 ms (66.75%) | Memory: 43.2 MB (33.50%) - LeetSync

---
+++ 1714-rearrange-spaces-between-words/rearrange-spaces-between-words.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reorderSpaces(String text) {
+        int spaces = 0;
+        for (char c : text.toCharArray()) {
+            if (c == ' ') spaces++;
+        }
+        String[] words = text.trim().split("\\s+");
+        if (words.length == 1) {
+            return words[0] + " ".repeat(spaces);
+        }
+        int gap = spaces / (words.length - 1);
+        int extra = spaces % (words.length - 1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+            if (i != words.length - 1) {
+                sb.append(" ".repeat(gap));
+            }
+        }
+        sb.append(" ".repeat(extra));
+        return sb.toString();
+    }
+}
