af56456b30e6300a9515efb0ee0a8532e777b73a
Ritul <ritul.srivastava007@gmail.com>
2026-06-14 19:35:06 +0530

    Time: 9 ms (10.03%) | Memory: 44.1 MB (10.69%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -3,14 +3,12 @@ class Solution {
         int range = s.length() / 2;
         int f1 = 0, f2 = 0;
         for (int i = 0; i < range; i++) {
-            char ch = s.charAt(i);
-            if ("aeiouAEIOU".indexOf(ch) != -1) {
+            if(s.charAt(i)=='a' || s.charAt(i)=='A' || s.charAt(i)=='e' || s.charAt(i)=='E' || s.charAt(i)=='i' || s.charAt(i)=='I' || s.charAt(i)=='o' || s.charAt(i)=='O' || s.charAt(i)=='u' || s.charAt(i)=='U'){
                 f1++;
             }
         }
         for (int i = range; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if ("aeiouAEIOU".indexOf(ch) != -1) {
+            if(s.charAt(i)=='a' || s.charAt(i)=='A' || s.charAt(i)=='e' || s.charAt(i)=='E' || s.charAt(i)=='i' || s.charAt(i)=='I' || s.charAt(i)=='o' || s.charAt(i)=='O' || s.charAt(i)=='u' || s.charAt(i)=='U'){
                 f2++;
             }
         }
