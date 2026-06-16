eab8d3ff95b0e854e1e4586a15d187b3b0ce45f3
Ritul <ritul.srivastava007@gmail.com>
2026-06-16 19:45:35 +0530

    Time: 2 ms (40.38%) | Memory: 43.5 MB (21.68%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,10 +1,11 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         String[] words = s.split(" ");
-        String[] ans = new String[k];
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < k; i++) {
-            ans[i] = words[i];
+            if (i > 0) sb.append(" ");
+            sb.append(words[i]);
         }
-        return String.join(" ", ans);
+        return sb.toString();
     }
 }
