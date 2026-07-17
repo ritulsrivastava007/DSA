05668ad6ccb9aeb6c2c2821936266a956d0d1c20
Ritul <ritul.srivastava007@gmail.com>
2026-07-17 20:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (34.09%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        boolean[] seen = new boolean[26];
+        for (char ch : s.toCharArray()) {
+            if (seen[ch - 'a']) {
+                return ch;
+            }
+            seen[ch - 'a'] = true;
+        }
+        return ' '; 
+    }
+}
