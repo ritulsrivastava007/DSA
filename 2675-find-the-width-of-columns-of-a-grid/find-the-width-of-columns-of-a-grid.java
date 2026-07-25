fcdfa0918fe79623af8b07a6b9cce4488683fd28
Ritul <ritul.srivastava007@gmail.com>
2026-07-25 23:27:53 +0530

    Time: 4 ms (75.20%) | Memory: 46.8 MB (97.97%) - LeetSync

--- 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.java
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.java
@@ -4,11 +4,23 @@ class Solution {
         for (int col = 0; col < grid[0].length; col++) {
             int max = 0;
             for (int row = 0; row < grid.length; row++) {
-                int len = String.valueOf(grid[row][col]).length();
-                max = Math.max(max, len);
+                max = Math.max(max, length(grid[row][col]));
             }
             ans[col] = max;
         }
         return ans;
+    }
+    private int length(int num) {
+        if (num == 0) return 1;
+        int count = 0;
+        if (num < 0) {
+            count++;       
+            num = -num;
+        }
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+        return count;
     }
 }
