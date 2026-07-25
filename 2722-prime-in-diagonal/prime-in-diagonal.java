1d707e5274f91dbfcdd579a0abfac5e100fa08da
Ritul <ritul.srivastava007@gmail.com>
2026-07-25 23:14:20 +0530

    Time: 5 ms (60.89%) | Memory: 82.3 MB (78.72%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int diagonalPrime(int[][] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (isPrime(nums[i][i])) {
+                ans = Math.max(ans, nums[i][i]);
+            }
+            if (isPrime(nums[i][nums.length - i - 1])) {
+                ans = Math.max(ans, nums[i][nums.length - i - 1]);
+            }
+        }
+        return ans;
+    }
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
