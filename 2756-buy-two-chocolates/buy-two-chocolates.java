373879cee97a2e8a01206c618e9e6e946c5cdead
Ritul <ritul.srivastava007@gmail.com>
2026-07-27 23:01:49 +0530

    Time: 3 ms (54.96%) | Memory: 46.4 MB (43.80%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int cost = prices[0] + prices[1];
+        if (cost > money) {
+            return money;
+        }
+        return money - cost;
+    }
+}
