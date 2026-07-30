4404f93550739993679fbd64ac034de2e8732444
Ritul <ritul.srivastava007@gmail.com>
2026-07-30 19:27:38 +0530

    Time: 3 ms (98.23%) | Memory: 46.6 MB (48.89%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder ans = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (ch == 'i') {
+                ans.reverse();
+            } else {
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
