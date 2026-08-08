eb71d608df5ddc08f3acc447627878659966a5e3
Ritul <ritul.srivastava007@gmail.com>
2026-08-08 19:10:38 +0530

    Time: 22 ms (100.00%) | Memory: 144.5 MB (13.89%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,104 +1,47 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        HashMap<Character, ArrayList<Integer>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(word1.charAt(i),
-                    k -> new ArrayList<>()).add(i);
-        }
-        int[] right = new int[m + 1];
-        right[m] = n;
-        for (int i = m - 1; i >= 0; i--) {
-            ArrayList<Integer> list = map.get(word2.charAt(i));
-            if (list == null) {
-                right[i] = -1;
-                continue;
-            }
-            int p = lowerBound(list, right[i + 1]);
-            if (p == 0) {
-                right[i] = -1;
+        char[] s = word1.toCharArray();
+        char[] t = word2.toCharArray();
+        int n = s.length;
+        int m = t.length;
+        int[] suffix = new int[n + 1];
+        int j = m - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (j >= 0 && s[i] == t[j]) {
+                suffix[i] = suffix[i + 1] + 1;
+                j--;
             } else {
-                right[i] = list.get(p - 1);
+                suffix[i] = suffix[i + 1];
             }
         }
         int[] ans = new int[m];
-        int prev = -1;
-        boolean usedMismatch = false;
-        for (int i = 0; i < m; i++) {
-            ArrayList<Integer> list =
-                    map.get(word2.charAt(i));
-
-            int exact = -1;
-            if (list != null) {
-                int p = upperBound(list, prev);
-                if (p < list.size()) {
-                    exact = list.get(p);
-                }
-            }
-            int mismatch = -1;
-            if (!usedMismatch) {
-                int start = prev + 1;
-                int end;
-                if (i + 1 == m) {
-                    end = n - 1;
-                } else {
-                    if (right[i + 1] == -1) {
-                        end = -1;
-                    } else {
-                        end = right[i + 1] - 1;
-                    }
-                }
-                if (end >= start) {
-                    for (int j = start; j <= end; j++) {
-                        if (word1.charAt(j) != word2.charAt(i)) {
-                            mismatch = j;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (exact != -1 && mismatch != -1) {
-                if (exact < mismatch) {
-                    ans[i] = exact;
-                } else {
-                    ans[i] = mismatch;
-                    usedMismatch = true;
-                }
-            } else if (exact != -1) {
-                ans[i] = exact;
-            } else if (mismatch != -1) {
-                ans[i] = mismatch;
-                usedMismatch = true;
+        int i = 0;
+        j = 0;
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                ans[j] = i;
+                j++;
             } else {
-                return new int[0];
+                if (suffix[i + 1] >= m - j - 1) {
+                    ans[j] = i;
+                    j++;
+                    i++;
+                    break;
+                }
             }
-            prev = ans[i];
+            i++;
         }
+        if (j < m && i == n)
+            return new int[0];
+        while (i < n && j < m) {
+            if (s[i] == t[j]) {
+                ans[j] = i;
+                j++;
+            }
+            i++;
+        }
+        if (j != m)
+            return new int[0];
         return ans;
-    }
-    private int upperBound(ArrayList<Integer> list, int target) {
-        int l = 0, r = list.size();
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (list.get(mid) <= target) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
-        }
-        return l;
-    }
-    private int lowerBound(ArrayList<Integer> list, int target) {
-        int l = 0, r = list.size();
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (list.get(mid) < target) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
-        }
-        return l;
     }
 }
