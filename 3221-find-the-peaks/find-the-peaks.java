1d6abe327a0d21423b80ca36b1324e6807fdd28e
Ritul <ritul.srivastava007@gmail.com>
2026-08-11 21:32:14 +0530

    Time: 1 ms (99.67%) | Memory: 45.7 MB (5.54%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> peak = new ArrayList<>();
+        for (int i = 1; i < mountain.length - 1; i++) {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+                peak.add(i);
+                i++;
+            }
+        }
+        return peak;
+    }
+}
